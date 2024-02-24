66a6e6fa1fc0b850d46d968acb30893ab4aaa2ce
Lily Whitford <lilywhitford2@gmail.com>
2024-02-24 05:27:21 +0000

    remove accidental extra function

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const { nextISSTimesForMyLocation } = require("./iss");
 
-// nextISSTimesForMyLocation((error, passTimes) => {
-//   if (error) {
-//     return console.log("It didn't work!", error);
-//   }
-
-//   console.log(passTimes);
-// });
-
 // fetchMyIP((error, ip) => {
 //   if (error) {
 //     console.log("It didn't work!", error);
